54eea9159195897ba26e13cc91a7468b6ced0e8b
Mayank <mayanksh784@gmail.com>
2025-04-09 17:47:13 +0530

    Added 02_arrays.js

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,38 @@
+// const marvel_heroes = ["thor", "Ironman", "Spiderman"];
+// const dc_heroes = ["superman", "flash", "batman"];
+
+// marvel_heroes.push(dc_heroes); //dc heroes array will go as an element is the marvel_heroes array
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]); //third element (i.e. dc_heroes array) ka first element i.e. flash
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const marvel_heroes = ["thor", "Ironman", "Spiderman"];
+// const dc_heroes = ["superman", "flash", "batman"];
+// const all_heroes = marvel_heroes.concat(dc_heroes)
+// console.log(all_heroes);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const marvel_heroes = ["thor", "Ironman", "Spiderman"];
+// const dc_heroes = ["superman", "flash", "batman"];
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
+
+// +++++++++++ using flat +++++++++++++++++
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+// const real_another_array = another_array.flat(Infinity);
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Hitesh")); //Hitesh is not an array, so false
+// console.log(Array.from("Mayan7%"));
+// console.log(Array.from({name: "Mayank"})); //interesting
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+// let score1 = 100 ;
+// let score2 = 200 ;
+// let score3 = 300 ;
+// console.log(Array.of(score1, score2, score3));
