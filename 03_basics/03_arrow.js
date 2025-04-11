8d8d4fcbe60eff44090705d9130e6b78e35bcf56
Mayank <mayanksh784@gmail.com>
2025-04-11 23:15:13 +0530

    done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,9 +1,16 @@
 const user = {
-    username : "Mayank",
-    price : 999,
+  username: "Mayank",
+  price: 999,
 
-    welcomeMessage : function() {
-        console.log(`${this.username}, Welcome to website!`);
-        
-    }
-}
+  welcomeMessage: function () {
+    console.log(`${this.username}, Welcome to website!`);
+    // console.log(this);
+    
+  },
+};
+//this means to refer to current context, us context mein username tha Mayank, but then it changed to Sharma.
+// user.welcomeMessage();
+// user.username = "Sharma"
+// user.welcomeMessage()
+
+console.log(this);
