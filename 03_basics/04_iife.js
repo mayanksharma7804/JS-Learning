e79e332c3b692a84564f4d07d57b293c3f38e500
Mayank <mayanksh784@gmail.com>
2025-04-11 23:44:26 +0530

    04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,15 @@
-// IIFE - Immediately Invoked Function Expression
+// IIFE - Immediately Invoked Function Expression : the function that will be immediately executed.
+// WHY Used? - the pollution caused by global scope, so to remove those problems, we use IIFE.
+
+//NAMED IIFE
+
+(function chai() { 
+  console.log(`DB CONNECTED`);
+})(); //end mein semicolon is imp to tell the function to stop here! 
+//line 3 to line 6 will print without even calling the function!
+
+//  +++++++++++++++++++++++++++++++++++++++
+
+((name) => {
+  console.log(`DB CONNECTED TO ${name}`);
+})(`Mayank`);
