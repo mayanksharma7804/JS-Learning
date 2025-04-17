81a428a00cb9d5a8a3a4d1a9a5ddd243dbcfb289
Mayank <mayanksh784@gmail.com>
2025-04-17 18:51:00 +0530

    new folder

---
+++ 04_control_flow/one.js
@@ -0,0 +1,33 @@
+// if 
+
+// if (true) {
+//     code will be executed if condition is true
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const isUserLoggedin = true 
+// if (isUserLoggedin) {
+//     console.log(`yes the user is logged in!`);
+    
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// if (2 == "2") {
+//     console.log(`yes the user is logged in!`);
+    
+// }
+//use === if you want to check if the both are of same type or not
+
+const temperature = 41
+if (temperature < 50) {
+    console.log("Less than 50.");
+    
+}
+else {
+    console.log("Temperature is greater than 50!");
+}
+console.log("Execute!");
+
+// <, >, <=, >=, ==, !=
