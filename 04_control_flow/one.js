9510fd11dcff1c52f8a0f2e5bfea05a50818c854
Mayank <mayanksh784@gmail.com>
2025-04-17 20:07:21 +0530

    added one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -20,14 +20,69 @@
 // }
 //use === if you want to check if the both are of same type or not
 
-const temperature = 41
-if (temperature < 50) {
-    console.log("Less than 50.");
+// const temperature = 41
+// if (temperature < 50) {
+//     console.log("Less than 50.");
     
-}
-else {
-    console.log("Temperature is greater than 50!");
-}
-console.log("Execute!");
+// }
+// else {
+//     console.log("Temperature is greater than 50!");
+// }
+// console.log("Execute!");
 
 // <, >, <=, >=, ==, !=
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// const score = 200 
+// if (score > 100) {
+//     var power = "fly" //scope of var is global, power variable's value can be accessed out of if too!
+//     console.log(`User Power : ${power}`);
+// }
+// console.log(`User Power : ${power}`);
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// shorthand notation
+
+// const balance = 1000 
+// if (balance > 500) console.log("test"); //implicit scope
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// const balance = 200 
+// if (balance < 300) console.log("test1"), console.log("test2"); //don't do like this!!, not a good practice
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// const balance = 800
+// if (balance < 500) {
+//     console.log("Less than 500");
+// }
+// else if (balance < 750) {
+//     console.log("Less than 750");
+    
+// }
+// else if (balance < 900) {
+//     console.log("Less than 900");
+    
+// }
+// else {
+//     console.log("less than 1200");
+    
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+const userLoggedin = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedin && debitCard) {
+    console.log("User is allowed to buy course.");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User is logged in.");
+}
