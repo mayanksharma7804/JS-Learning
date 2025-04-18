8b90f811dab65e5653b0e916074e0f68f651d40a
Mayank <mayanksh784@gmail.com>
2025-04-18 15:34:52 +0530

    switch.js

--- 04_control_flow/02_switch.js
+++ 04_control_flow/02_switch.js
@@ -1,7 +1,7 @@
-// switch syntax : 
+// switch syntax :
 // switch (key) {
 //     case value:
-        
+
 //         break;
 
 //     default:
@@ -10,24 +10,24 @@
 
 // ++++++++++++++++++++++++++++++++++++++++++++
 
-const month = 3 //in place of 3, if there would have been "jan", then later would be case "jan": 
+const month = 3; //in place of 3, if there would have been "jan", then later would be case "jan":
 
 switch (month) {
-    case 1:
-        console.log("January");
-        break;
-    case 2:
-        console.log("February");
-        break;
-    case 3:
-        console.log("March");
-        break;
-    case 4:
-        console.log("April");
-        break;
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
 
-    default:
-        console.log("not matched month");
-        
-        break;
+  default:
+    console.log("not matched month");
+
+    break;
 }
